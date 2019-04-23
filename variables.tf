cc118a696f409d17303fc15c03a32aec15ef06ba
afrank <afrank@mozilla.com>
2019-04-23 14:33:49 -0700

    moving gke tf code to a module

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "google-generated Project ID"
+}
+
+variable "cluster_name" {
+  decription = "the name you want your cluster to have"
+}
+
+variable "region" {
+  default = "us-west1"
+}
+
+variable "creds-file" {
+  description = "creds file downloaded from Google"
+}
+
+variable "ssh_username" { }
+variable "ssh_password" { }
+
